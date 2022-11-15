10044e96c834c4560d06099381188662f8c8d4c7
ziwei zhang <ziwei632@gmail.com>
2022-11-15 10:36:41 -0500

    d

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -8,7 +8,10 @@ export default async function handler(req, res) {
   const session = getSession(req, res);
   const user = session?.user;
   if (user) {
-    const stripeId = user["http://localhost:3000/stripe_customer_id"];
+    const stripeId =
+      user[
+        "https://https://luxury-frontend-chi.vercel.app//stripe_customer_id"
+      ];
     if (req.method === "POST") {
       try {
         const session = await stripe.checkout.sessions.create({
